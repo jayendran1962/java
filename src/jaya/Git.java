7d2657c9ad0adbcc364e7415b62d3bb4edf9b301
jayendran sethuraman <jayendran1708@gmail.com>
2022-01-20 19:30:15 +0530

    java prg

---
+++ src/jaya/Git.java
@@ -0,0 +1,15 @@
+package jaya;
+
+public class Git {
+	
+	private void empId() {
+		System.out.println("employee ID is :");
+		
+	}
+	public static void main(String[] args) {
+		Git a = new Git();
+		a.empId();
+	}
+	
+
+}
