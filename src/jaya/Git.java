f77d406b84fc886f6e6dd1dc6ef8b7b3bef6c2fa
jayendran sethuraman <jayendran1708@gmail.com>
2022-01-20 19:42:33 +0530

    new java

---
+++ src/jaya/Git.java
@@ -0,0 +1,13 @@
+package jaya;
+
+public class Git {
+	
+private void empNme() {
+System.out.println("employee name is :");
+
+}
+public static void main(String[] args) {
+	Git a = new Git();
+	a.empNme();
+}
+}
